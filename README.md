2b6fef9b07f86c6307e6d161ecfbda1e3684eed4
Raymond Zhou <raymondzhou2019@gmail.com>
2021-01-16 14:07:58 -0500

    Imported files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FolloWaldo
+
+1. Run [run_inference.py](run_inference.py) to perform inference
+    * Update image_paths if changing images
+2. Images can be updated in [images/input](images/input) and are produced in [images/output](images/output)
+3. Model is saved in [exported-models/my_model](exported-models/my_model)
---
+++ run_inference.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+
+PATH_TO_SAVED_MODEL = "./exported-models/my_model/saved_model"
+PATH_TO_LABEL_MAP = "./annotations/label_map.pbtxt"
+image_paths = ["/1.jpg", "./2.jpg", "./3.jpg", "./4.jpg", "./5.jpg", "./6.jpg", "./7.jpg"]
+
+
+def inference():
+    print("Loading model... ", end='')
+
+    # Load saved model and build the detection function
+    detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
+
+    # Loading the label_map
+    category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABEL_MAP, use_display_name=True)
+
+    print("Done!")
+
+    def load_image_into_numpy_array(path):
+        # Load an image from file into a numpy array.
+        return np.array(Image.open(path))
+
+    for image_path in image_paths:
+        print("Running inference for {}... ".format(image_path), end='')
+        image_np = load_image_into_numpy_array("./images/input" + image_path)
+
+        # The input needs to be a tensor, convert it using `tf.convert_to_tensor`
+        input_tensor = tf.convert_to_tensor(image_np)
+        # The model expects a batch of images, so add an axis with `tf.newaxis`
+        input_tensor = input_tensor[tf.newaxis, ...]
+
+        # input_tensor = np.expand_dims(image_np, 0)
+        detections = detect_fn(input_tensor)
+
+        # Convert to numpy arrays, and take index [0] to remove the batch dimension
+        num_detections = int(detections.pop('num_detections'))
+        detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
+        detections['num_detections'] = num_detections
+
+        # Detection_classes should be ints
+        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+        image_np_with_detections = image_np.copy()
+
+        viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes'],
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=1,
+            min_score_thresh=.1,
+            agnostic_mode=False)
+
+        image = Image.fromarray(image_np_with_detections)
+        image.save("./images/output" + image_path)
+        print("Done!")
+
+
+if __name__ == "__main__":
+    inference()
